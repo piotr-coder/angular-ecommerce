4bb4fffbeb4bf48e3ddc292c4e4dae4831c4d3cd
piotr-coder <blichu@wp.pl>
2021-07-02 13:32:23 +0200

    Create new Product Details Component

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -27,6 +27,12 @@ export class ProductService {
     return this.getProducts(searchUrl);
   }
 
+  getProduct(theProductId: number): Observable<Product> {
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+
+    return this.httpClient.get<Product>(productUrl);
+  }
+  
   private getProducts(searchUrl: string): Observable<Product[]> {
     return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
