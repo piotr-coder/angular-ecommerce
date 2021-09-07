7208fbf6c02f546f45f16f95ed482c0c79e37811
piotr-coder <blichu@wp.pl>
2021-09-07 15:32:22 +0200

    Angular Environment Configuration

--- src/app/services/date-shop-form.service.ts
+++ src/app/services/date-shop-form.service.ts
@@ -4,14 +4,15 @@ import { HttpClient } from '@angular/common/http';
 import { Country } from '../common/country';
 import { map } from 'rxjs/operators';
 import { State } from '../common/state';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DateShopFormService {
 
-  private countriesUrl = 'http://localhost:8080/api/countries';
-  private statesUrl = 'http://localhost:8080/api/states';
+  private countriesUrl = environment.pbshopApiUrl + '/countries';
+  private statesUrl = environment.pbshopApiUrl + '/states';
 
   constructor(private httpClient: HttpClient) { }
 
